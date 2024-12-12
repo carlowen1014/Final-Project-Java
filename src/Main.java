9f3cc5890810d154f5e1eb48406a333e94e195af
carlowen1014 <cowen8@cub.uca.edu>
2024-12-11 19:03:13 -0600

    Minor adjustment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 //Carl Owen, Ty Parham, Alex Kiss
 //Semester Project main
-//The purpose of the program
+/*The purpose of the program application is to track your monthly
+income, expenses and savings and tell you if you are on track based on previous
+contingicies, it takes the dates of your interactions with the account and also
+keeps your data and you have a login that is compatiable
+for testing you can use the admin and password for username and password
+*/
 
 public class Main {
     public static void main(String[] args) {
